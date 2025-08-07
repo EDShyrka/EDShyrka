7cf684e01a4f37aa8843bde0da225fa343859b5d
tbayart <tbayart@nordnet.fr>
2025-08-07 18:46:45 +0200

    Refactored browser hosting

---
+++ src/EDShyrka/BrowserHosting.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EDShyrka
+{
+    internal static class BrowserHosting
+    {
+        public static Task StartAsync(string[] args, out CancellationTokenSource cancellationTokenSource)
+        {
+            // Set the content root to the wwwroot directory where static files are served from.
+            var contentRoot = System.IO.Path.Combine(AppContext.BaseDirectory, @"wwwroot");
+            var webApplicationOptions = new WebApplicationOptions { Args = args };
+            var builder = WebApplication.CreateBuilder(webApplicationOptions);
+            builder.WebHost.UseKestrelCore().ConfigureKestrel(ConfigureKestrel);
+
+            var app = builder.Build();
+            app.UseDefaultFiles(new DefaultFilesOptions { FileProvider = new PhysicalFileProvider(contentRoot) });
+            app.UseStaticFiles(new StaticFileOptions { FileProvider = new PhysicalFileProvider(contentRoot), ServeUnknownFileTypes = true });
+
+            app.MapGet("/hello", () => $"{DateTime.Now:hh:mm:ss} - Welcome to EDShyrka !");
+            app.UseWebSockets(new WebSocketOptions { });
+
+            cancellationTokenSource = new CancellationTokenSource();
+            return app.StartAsync(cancellationTokenSource.Token);
+        }
+
+        private static void ConfigureKestrel(KestrelServerOptions options)
+        {
+            options.ListenAnyIP(12080);
+        }
+    }
+}
--- src/EDShyrka/Program.cs
+++ src/EDShyrka/Program.cs
@@ -1,11 +1,6 @@
-﻿using System;
+﻿using Avalonia;
+using System;
 using System.Threading.Tasks;
-using Avalonia;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.FileProviders;
-using System.Reflection;
-using System.Diagnostics;
 
 namespace EDShyrka;
 
@@ -17,43 +12,16 @@ sealed class Program
     [STAThread]
     public static async Task Main(string[] args)
     {
-        _ = StartBrowserHosting(args);
+        // Start the browser hosting server.
+        var hostingTask = BrowserHosting.StartAsync(args, out var hostingCancellationTokenSource);
 
+        // The hosting task is started, now we can run the Avalonia application.
         BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
-    }
 
-    private static Task StartBrowserHosting(string[] args)
-    {
-        try
-        {
-            //var contentRoot = System.IO.Path.Combine(AppContext.BaseDirectory, @"../../../../EDShyrka.Browser/bin/Release/net9.0-browser/publish/wwwroot");
-            var contentRoot = System.IO.Path.Combine(AppContext.BaseDirectory, @"wwwroot");
-            var webApplicationOptions = new WebApplicationOptions { Args = args };
-            var builder = WebApplication.CreateBuilder(webApplicationOptions);
-            builder.WebHost
-                .UseKestrelCore()
-                .ConfigureKestrel(ConfigureKestrel);
-
-            var app = builder.Build();
-            app.UseDefaultFiles(new DefaultFilesOptions { FileProvider = new PhysicalFileProvider(contentRoot) });
-            app.UseStaticFiles(new StaticFileOptions { FileProvider = new PhysicalFileProvider(contentRoot), ServeUnknownFileTypes = true });
-            app.MapGet("/hello", () => $"{DateTime.Now:hh:mm:ss} - Welcome to EDShyrka !");
-            //app.UseWebSockets()
-            return app.StartAsync();
-
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error starting browser hosting: {ex.Message}");
-            Debugger.Break();
-            return Task.CompletedTask;
-        }
-    }
-
-    private static void ConfigureKestrel(Microsoft.AspNetCore.Server.Kestrel.Core.KestrelServerOptions options)
-    {
-        options.ListenAnyIP(12080);
+        // stop the hosting task gracefully when the application exits.
+        hostingCancellationTokenSource.Cancel();
+        await hostingTask;
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
