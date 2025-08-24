e1cd824546ee4e78c556b59f0034d267a7b5f445
tbayart <tbayart@nordnet.fr>
2025-08-24 21:23:39 +0200

    Added logging to Shyrka.UI

--- src/EDShyrka.UI/App.axaml.cs
+++ src/EDShyrka.UI/App.axaml.cs
@@ -1,54 +1,69 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
-using System.Linq;
 using Avalonia.Markup.Xaml;
+using EDShyrka.Logging;
 using EDShyrka.UI.ViewModels;
 using EDShyrka.UI.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
 
 namespace EDShyrka.UI;
 
 public partial class App : Application
 {
-    public override void Initialize()
-    {
-        AvaloniaXamlLoader.Load(this);
-    }
+	private ServiceProvider _services;
 
-    public override void OnFrameworkInitializationCompleted()
-    {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            DisableAvaloniaDataAnnotationValidation();
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainViewModel()
-            };
-        }
-        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
-        {
-            singleViewPlatform.MainView = new MainView
-            {
-                DataContext = new MainViewModel()
-            };
-        }
+	public override void Initialize()
+	{
+		ConfigureServices();
+		AvaloniaXamlLoader.Load(this);
+	}
 
-        base.OnFrameworkInitializationCompleted();
-    }
+	private void ConfigureServices()
+	{
+		var collection = new ServiceCollection();
+		collection.AddLogging(o => o.ConfigureLogging());
+		collection.AddSingleton<MainViewModel>();
+		_services = collection.BuildServiceProvider();
+	}
 
-    private void DisableAvaloniaDataAnnotationValidation()
-    {
-        // Get an array of plugins to remove
-        var dataValidationPluginsToRemove =
-            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
+	public override void OnFrameworkInitializationCompleted()
+	{
+		var mainViewModel = _services.GetRequiredService<MainViewModel>();
+		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+		{
+			// Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
+			// More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
+			DisableAvaloniaDataAnnotationValidation();
 
-        // remove each entry found
-        foreach (var plugin in dataValidationPluginsToRemove)
-        {
-            BindingPlugins.DataValidators.Remove(plugin);
-        }
-    }
+			desktop.MainWindow = new MainWindow
+			{
+				DataContext = mainViewModel
+			};
+		}
+		else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+		{
+			singleViewPlatform.MainView = new MainView
+			{
+				DataContext = mainViewModel
+			};
+		}
+
+		base.OnFrameworkInitializationCompleted();
+	}
+
+	private void DisableAvaloniaDataAnnotationValidation()
+	{
+		// Get an array of plugins to remove
+		var dataValidationPluginsToRemove =
+			BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
+
+		// remove each entry found
+		foreach (var plugin in dataValidationPluginsToRemove)
+		{
+			BindingPlugins.DataValidators.Remove(plugin);
+		}
+	}
 }
--- src/EDShyrka.UI/ViewModels/MainViewModel.cs
+++ src/EDShyrka.UI/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Diagnostics;
 using System.Windows.Input;
@@ -8,41 +9,46 @@ namespace EDShyrka.UI.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    private int _counter = 0;
+	private readonly ILogger _logger;
 
-    public MainViewModel()
-    {
-        if (OperatingSystem.IsBrowser() == false)
-        {
-            Greeting = "Welcome to EDShyrka Desktop !";
-            _clickMeCommand = new RelayCommand(LaunchBrowser);
-        }
-        else
-        {
-            Greeting = $"Welcome to EDShyrka Browser ! (host is {JSInterop.getHostAddress()})";
-            _clickMeCommand = new RelayCommand(IncrementCounter);
-        }
-    }
+	private int _counter = 0;
 
-    private void LaunchBrowser()
-    {
-        var uriBuilder = new UriBuilder("http", "localhost", 12080);
-        Process.Start(new ProcessStartInfo { FileName = uriBuilder.Uri.ToString(), UseShellExecute = true });
-        ClickMeCommand = new RelayCommand(IncrementCounter);
-    }
+	public MainViewModel(ILogger<MainViewModel> logger)
+	{
+		_logger = logger;
+		if (OperatingSystem.IsBrowser() == false)
+		{
+			Greeting = "Welcome to EDShyrka Desktop !";
+			_clickMeCommand = new RelayCommand(LaunchBrowser);
+		}
+		else
+		{
+			Greeting = $"Welcome to EDShyrka Browser ! (host is {JSInterop.getHostAddress()})";
+			_clickMeCommand = new RelayCommand(IncrementCounter);
+		}
+		_logger.Log(LogLevel.Information, "Started");
+	}
 
-    public string Greeting { get; }
+	private void LaunchBrowser()
+	{
+		var uriBuilder = new UriBuilder("http", "localhost", 12080);
+		Process.Start(new ProcessStartInfo { FileName = uriBuilder.Uri.ToString(), UseShellExecute = true });
+		ClickMeCommand = new RelayCommand(IncrementCounter);
+	}
 
-    [ObservableProperty]
-    private string _labelText = "Hello, World!";
+	public string Greeting { get; }
+
+	[ObservableProperty]
+	private string _labelText = "Hello, World!";
 
 
-    [ObservableProperty]
-    private ICommand _clickMeCommand;
+	[ObservableProperty]
+	private ICommand _clickMeCommand;
 
-    [RelayCommand]
-    public void IncrementCounter()
-    {
-        LabelText = $"Clicked {++_counter} times !";
-    }
+	[RelayCommand]
+	public void IncrementCounter()
+	{
+		LabelText = $"Clicked {++_counter} times !";
+		_logger.Log(LogLevel.Information, "Button clicked {count} times", _counter);
+	}
 }
