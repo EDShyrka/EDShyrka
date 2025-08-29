97a3d0a6cfcfae40ee80613064c6a6ca3f08ed25
tbayart <tbayart@nordnet.fr>
2025-08-29 18:46:44 +0200

    Added ViewModels/ViewModelProvider.cs

---
+++ src/EDShyrka.UI/ViewModels/ViewModelProvider.cs
@@ -0,0 +1,57 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+using System;
+
+namespace EDShyrka.UI.ViewModels
+{
+    /// <summary>
+    /// Markup extension responsible for providing a viewmodel instance.
+    /// </summary>
+    public class ViewModelProvider : MarkupExtension
+    {
+        #region ctor
+        /// <summary>
+        /// Initializes a <see cref="ViewModelProvider"/> instance.
+        /// </summary>
+        public ViewModelProvider()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a <see cref="ViewModelProvider"/> instance.
+        /// </summary>
+        /// <param name="viewModelType">The type of requested viewmodel.</param>
+        public ViewModelProvider(Type viewModelType)
+        {
+            ViewModelType = viewModelType;
+        }
+        #endregion ctor
+
+        #region properties
+        /// <summary>
+        /// The type of viewmodel for runtime.
+        /// </summary>
+        public Type ViewModelType { get; set; }
+
+        /// <summary>
+        /// The type of viewmodel for design time.
+        /// </summary>
+        public Type DesignViewModelType { get; set; }
+        #endregion properties
+
+        #region methods
+        /// <summary>
+        /// Provide the instance of viewmodel.
+        /// </summary>
+        /// <param name="serviceProvider">A service provider helper that can provide services for the markup extension.</param>
+        /// <returns>A viewmodel instance.</returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (AppHelpers.IsDesignMode)
+                return DesignViewModelType ?? Activator.CreateInstance(DesignViewModelType);
+
+			return ((App)Application.Current).ServiceProvider.GetService(ViewModelType);
+        }
+        #endregion methods
+    }
+}
