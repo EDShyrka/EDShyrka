5727b2ae430a25bc7b0f5527f4352796edb476b1
tbayart <tbayart@nordnet.fr>
2025-08-29 18:25:54 +0200

    Added Mutex mechanism to ensure unique instance is running

--- src/EDShyrka/Program.cs
+++ src/EDShyrka/Program.cs
@@ -1,33 +1,70 @@
 ﻿using Avalonia;
+using EDShyrka.Logging;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EDShyrka;
 
 sealed class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
-    [STAThread]
-    public static async Task Main(string[] args)
-    {
-        // Start the browser hosting server.
-        var hostingTask = BrowserHosting.StartAsync(args, out var hostingCancellationTokenSource);
+	private const string _uniqueApplicationIdentifier = "global://EDShyrka/4fcd696e917c68e82a99f556541a350e";
+	private static Mutex? _singleInstanceMutex;
 
-        // The hosting task is started, now we can run the Avalonia application.
-        BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+	// Initialization code. Don't use any Avalonia, third-party APIs or any
+	// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+	// yet and stuff might break.
+	[STAThread]
+	public static async Task Main(string[] args)
+	{
+		if (IsSingleInstance() == false)
+		{
+			LoggingHelpers.LoggerFactory.CreateLogger("EDShyrka")
+				.Log(LogLevel.Warning, "Another instance is already running, closing");
+			Environment.Exit(0);
+		}
 
-        // stop the hosting task gracefully when the application exits.
-        hostingCancellationTokenSource.Cancel();
-        await hostingTask;
-    }
+		// Start the browser hosting server.
+		var hostingTask = BrowserHosting.StartAsync(args, out var hostingCancellationTokenSource);
 
-    // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<UI.App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace();
+		// The hosting task is started, now we can run the Avalonia application.
+		BuildAvaloniaApp()
+			.StartWithClassicDesktopLifetime(args);
+
+		// stop the hosting task gracefully when the application exits.
+		hostingCancellationTokenSource.Cancel();
+		await hostingTask;
+	}
+
+	// Avalonia configuration, don't remove; also used by visual designer.
+	public static AppBuilder BuildAvaloniaApp()
+		=> AppBuilder.Configure<UI.App>()
+			.UsePlatformDetect()
+			.WithInterFont()
+			.LogToTrace();
+
+	/// <summary>
+	/// Check for multiple instances
+	/// </summary>
+	/// <returns>True if the program can run, false if another instance is already running.</returns>
+	public static bool IsSingleInstance()
+	{
+		try
+		{
+			// Try to open existing mutex.
+			Mutex.OpenExisting(_uniqueApplicationIdentifier);
+		}
+		catch
+		{
+			// If exception occurred, there is no such mutex so create it.
+			_singleInstanceMutex = new Mutex(true, _uniqueApplicationIdentifier);
+
+			// Only one instance.
+			return true;
+		}
+
+		// More than one instance.
+		return false;
+	}
 }
