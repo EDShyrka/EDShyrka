c198e2e8a66ba21acadced81e66704a801b99c10
tbayart <tbayart@nordnet.fr>
2025-08-30 10:18:31 +0200

    Build fix : pushing missing EDShyrka.UI/AppHelpers.cs

---
+++ src/EDShyrka.UI/AppHelpers.cs
@@ -0,0 +1,41 @@
+﻿using Avalonia;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+
+namespace EDShyrka.UI
+{
+    /// <summary>
+    /// Helpers for global application management.
+    /// </summary>
+    public static class AppHelpers
+    {
+        private static readonly Lazy<string> _versionProvider = new(VersionProviderFactory);
+
+        private static string VersionProviderFactory()
+        {
+            if (IsDesignMode)
+                return "EDShyrka.UI - design mode v00.00.00.00000";
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var info = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return $"{info.ProductName} - v{info.FileVersion}";
+        }
+
+        /// <summary>
+        /// Application's version information to display.
+        /// </summary>
+        public static string VersionInfo { get => _versionProvider.Value; }
+
+        /// <summary>
+        /// Returns the display name of the assembly that contains the code that is currently executing.
+        /// </summary>
+        public static string ExecutingAssemblyFullName { get => Assembly.GetExecutingAssembly().FullName; }
+
+        /// <summary>
+        /// True if the WPF application is in design mode, false otherwise.
+        /// </summary>
+        public static bool IsDesignMode { get => Application.Current is App == false; }
+    }
+}
