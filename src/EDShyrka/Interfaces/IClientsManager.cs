51fd970f3e30017fa5077920b9af13fbed43546f
tbayart <tbayart@nordnet.fr>
2025-09-01 22:28:03 +0200

    Added Interfaces/IClientsManager.cs

---
+++ src/EDShyrka/Interfaces/IClientsManager.cs
@@ -0,0 +1,27 @@
+﻿using EDShyrka.Models;
+using System;
+using System.Collections.Generic;
+
+namespace EDShyrka.Interfaces
+{
+	public interface IClientsManager
+	{
+		public event ClientConnectedEventHandler ClientConnected;
+
+		public void RegisterClient(WebSocketClient client);
+
+		public IEnumerable<WebSocketClient> Client { get; }
+	}
+
+	public delegate void ClientConnectedEventHandler(object sender, ClientConnectedEventArgs args);
+
+	public class ClientConnectedEventArgs : EventArgs
+	{
+		public WebSocketClient Client { get; }
+
+		public ClientConnectedEventArgs(WebSocketClient client)
+		{
+			Client = client;
+		}
+	}
+}
