a63ed3e0addb55a262c13d40757e09388ad27cf0
tbayart <tbayart@nordnet.fr>
2025-09-07 23:01:17 +0200

    Updated Mvc/WebSocketController.cs to work with IClientsManager and WebSocketClient

--- src/EDShyrka/Mvc/WebSocketController.cs
+++ src/EDShyrka/Mvc/WebSocketController.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using EDShyrka.Interfaces;
 using EDShyrka.Mvc.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using System.Net.WebSockets;
+using System.Threading.Tasks;
 
 namespace EDShyrka.Mvc
 {
@@ -15,41 +12,26 @@ namespace EDShyrka.Mvc
 	[TypeFilter<WebSocketAuthorizationFilter>]
 	public class WebSocketController : ControllerBase
 	{
+		#region fields
 		private readonly ILogger _logger;
+		private readonly IClientsManager _clientsManager;
+		#endregion fields
 
-		public WebSocketController(ILogger<WebSocketController> logger)
+		#region ctor
+		public WebSocketController(ILogger<WebSocketController> logger, IClientsManager clientsManager)
 		{
 			_logger = logger;
+			_clientsManager = clientsManager;
 		}
+		#endregion ctor
 
+		#region methods
 		public async Task Get()
 		{
-			// accept the client connection
-			using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
-			_logger.Log(LogLevel.Information, "WebSocket client {clientId} connected", clientId);
-			while (webSocket.State == System.Net.WebSockets.WebSocketState.Open)
-			{
-				var buffer = new Memory<byte>(new byte[1024]);
-				var result = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
-				_logger.Log(LogLevel.Information, "WebSocket client sent {Count} bytes in a {MessageType} message (eof:{isEof})", result.Count, result.MessageType, result.EndOfMessage);
-				if (result.MessageType == System.Net.WebSockets.WebSocketMessageType.Close)
-				{
-					await webSocket.CloseAsync(System.Net.WebSockets.WebSocketCloseStatus.NormalClosure, "Closed by the WebSocketController", CancellationToken.None);
-					_logger.Log(LogLevel.Information, "WebSocket client {clientId} disconnected", clientId);
-					break;
-				}
-				else if (result.MessageType == System.Net.WebSockets.WebSocketMessageType.Binary)
-				{
-					buffer = buffer.Slice(0, result.Count);
-					await webSocket.SendAsync(buffer, System.Net.WebSockets.WebSocketMessageType.Binary, true, CancellationToken.None);
-					_logger.Log(LogLevel.Information, "Replying to client");
-				}
-
-				// keep the connection alive
-				await Task.Delay(100);
-			}
-
-			await Task.CompletedTask;
+			_logger.Log(LogLevel.Information, "Incoming WebSocket client");
+			using var client = await _clientsManager.ConnectClient(HttpContext.WebSockets);
+			await client.Listener;
 		}
+		#endregion methods
 	}
 }
