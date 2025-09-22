3e20993c3b2542b8ffaabded980ee9c8373c5ce1
tbayart <tbayart@nordnet.fr>
2025-09-22 08:23:07 +0200

    Added EDJournal/Models/EDJournalEntry.cs base model for all journal entries

---
+++ src/EDShyrka/EDJournal/Models/EDJournalEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EDShyrka.EDJournal.Models
+{
+    /// <summary>
+    /// Base class for all entries in journal
+    /// </summary>
+    public class EDJournalEntry
+    {
+		/// <summary>
+		/// The event's raw JSON data.
+		/// </summary>
+		public string RawData { get; set; }
+
+        /// <summary>
+        /// The entry timestamp.
+        /// </summary>
+        public DateTime Timestamp { get; set; }
+
+        /// <summary>
+        /// The event type.
+        /// </summary>
+        public string Event { get; set; }
+    }
+}
