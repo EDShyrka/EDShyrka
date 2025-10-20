97e125444d7d47ee8d45e8e34e558b4752a488cd
tbayart <tbayart@nordnet.fr>
2025-10-20 18:28:47 +0200

    Added EDShyrka/EDJournal/EDJournalFolder.cs

---
+++ src/EDShyrka/EDJournal/EDJournalFolder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace EDShyrka.EDJournal
+{
+	/// <summary>
+	/// Locates the Elite Dangerous journals folder and store the value.
+	/// </summary>
+	public class EDJournalFolder
+	{
+		private const string _subFolder = @"Saved Games\Frontier Developments\Elite Dangerous\";
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="EDJournalFolder"/> class.
+		/// </summary>
+		public EDJournalFolder()
+		{
+			// locate journal folder using special folder enum
+			var userProfileFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+			Value = Path.Combine(userProfileFolder, _subFolder);
+		}
+
+		/// <summary>
+		/// The Elite Dangerous journals folder.
+		/// </summary>
+		public string Value { get; init; }
+	}
+}
