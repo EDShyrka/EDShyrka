14d96da3e8cf312dc79ff16103ce99b20772a774
tbayart <tbayart@nordnet.fr>
2026-01-22 22:52:03 +0100

    Added EDShyrka/EDJournal/EDGameExecutable.cs

---
+++ src/EDShyrka/EDJournal/EDGameExecutable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace EDShyrka.EDJournal;
+
+/// <summary>
+/// Provide information about the Elite Dangerous game executable.
+/// </summary>
+public class EDGameExecutableInfo
+{
+	#region fields
+	private const string _exeName = "EliteDangerous64";
+	private Process? _edProcess;
+	#endregion fields
+
+	#region properties
+	public bool IsRunning
+	{
+		get
+		{
+			var process = GetProcess();
+			return process != null && process.HasExited == false;
+		}
+	}
+
+	public TimeSpan UpTime
+	{
+		get
+		{
+			var process = GetProcess();
+			return process != null
+				? DateTime.Now - process.StartTime
+				: TimeSpan.Zero;
+		}
+	}
+	#endregion properties
+
+	#region methods
+	private Process? GetProcess()
+	{
+		if (_edProcess == null || _edProcess.HasExited)
+		{
+			var processes = Process.GetProcessesByName(_exeName);
+			_edProcess = processes.FirstOrDefault();
+		}
+		return _edProcess;
+	}
+	#endregion methods
+}
