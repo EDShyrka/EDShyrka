633bbed842b166855f36ca221778de78a07788f9
tbayart <tbayart@nordnet.fr>
2026-01-22 22:38:03 +0100

    Added EDShyrka/EDJournal/Models/Fileheader.cs

---
+++ src/EDShyrka/EDJournal/Models/Fileheader.cs
@@ -0,0 +1,37 @@
+﻿namespace EDShyrka.EDJournal.Models
+{
+    /// <summary>
+    /// FileHeadser event written when game starts.
+    /// </summary>
+    public class Fileheader : EDJournalEntry
+    {
+        /// <summary>
+        /// Unknown information.
+        /// </summary>
+        public int Part { get; set; }
+
+        /// <summary>
+        /// The game's language.
+        /// </summary>
+        public string Language { get; set; }
+
+        /// <summary>
+        /// True if game is Odyssey, false otherwise.
+        /// </summary>
+        public bool Odyssey { get; set; }
+
+        /// <summary>
+        /// The game's version.
+        /// </summary>
+        public string GameVersion { get; set; }
+
+        /// <summary>
+        /// The game's build information.
+        /// </summary>
+        public string Build { get; set; }
+
+        #region samples
+        //{ "timestamp":"2022-01-18T09:24:42Z", "event":"Fileheader", "part":1, "language":"English\\UK", "Odyssey":false, "gameversion":"3.8.0.403", "build":"r279702/r0 " }
+        #endregion samples
+    }
+}
